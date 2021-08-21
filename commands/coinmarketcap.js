f004b72d4023fd0138899e4f47806f4aa5549d82
John <jmarquez.cs@gmail.com>
2021-08-21 18:26:31 -0400

    assets

--- commands/coinmarketcap.js
+++ commands/coinmarketcap.js
@@ -13,7 +13,7 @@ module.exports = async (msg, args) => {
     try {
       const config = {
         method: 'get',
-        url: 'http://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=1&convert=USD',
+        url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=1&convert=USD',
         headers: {
           'X-CMC_PRO_API_KEY': `${cmcKey}`
         }
@@ -21,16 +21,23 @@ module.exports = async (msg, args) => {
 
       let getData = async () => {
         let response = await axios(config);
-        console.log(`${response.data.data[0].quote.USD.price}`);
+        console.log(`${response.data.data[0]}`);
+        // console.log(`${response.data.data[0].quote.USD.price}`);
 
         let envelope = JSON.stringify(response.data.data[0].quote.USD.price);
         console.log(`envelope data: ${envelope}`);
-        return msg.channel.send(`Hi traveler, BTC is currently priced at: **${envelope} USD**`);
+
+        if (envelope) {
+          msg.channel.send(`BTC is currently priced at: **${envelope} USD**`);
+        } else {
+          return
+        }
       }
 
       let instantiation = await getData();
     } catch (err) {
       console.log(err);
+      return
     }
   }
 }
--- commands/run-flower-poker.js
+++ commands/run-flower-poker.js
@@ -1,15 +1,17 @@
-const { MessageButton } = require("discord-buttons");
-const { MessageEmbed } = require("discord.js");
+const { MessageAttachment } = require("discord.js")
+const { MessageButton } = require("discord-buttons")
+const fs = require('fs')
+const path = require('path')
 
-const pastelFlowers = 'https://static.wikia.nocookie.net/runescape2/images/7/7b/Flowers_%28pastel%29.png/revision/latest/scale-to-width-down/29?cb=20160502034448';
-const redFlowers = 'https://static.wikia.nocookie.net/runescape2/images/8/8d/Red_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502065631';
-const blueFlowers = 'https://static.wikia.nocookie.net/runescape2/images/5/59/Blue_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502014331';
-const yellowFlowers = 'https://static.wikia.nocookie.net/runescape2/images/b/b8/Yellow_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502092709';
-const purpleFlowers = 'https://static.wikia.nocookie.net/runescape2/images/c/c3/Purple_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502064716';
-const orangeFlowers = 'https://static.wikia.nocookie.net/runescape2/images/f/f9/Orange_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502061510';
-const mixedFlowers = 'https://static.wikia.nocookie.net/runescape2/images/9/9a/Flowers_%28mixed%29.png/revision/latest/scale-to-width-down/29?cb=20160502034446';
-const whiteFlowers = 'https://static.wikia.nocookie.net/runescape2/images/c/c3/White_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502091647';
-const blackFlowers = 'https://static.wikia.nocookie.net/runescape2/images/f/f1/Black_flowers.png/revision/latest/scale-to-width-down/29?cb=20160502013354';
+const pastelFlowers = fs.readFileSync(path.join(__dirname, '../assets/pastelFlowers.png'))
+const redFlowers = fs.readFileSync(path.join(__dirname, '../assets/redFlowers.png'))
+const blueFlowers = fs.readFileSync(path.join(__dirname, '../assets/blueFlowers.png'))
+const yellowFlowers = fs.readFileSync(path.join(__dirname, '../assets/yellowFlowers.png'))
+const purpleFlowers = fs.readFileSync(path.join(__dirname, '../assets/purpleFlowers.png'))
+const orangeFlowers = fs.readFileSync(path.join(__dirname, '../assets/orangeFlowers.png'))
+const mixedFlowers = fs.readFileSync(path.join(__dirname, '../assets/mixedFlowers.png'))
+const whiteFlowers = fs.readFileSync(path.join(__dirname, '../assets/whiteFlowers.png'))
+const blackFlowers = fs.readFileSync(path.join(__dirname, '../assets/blackFlowers.png'))
 
 const flowers = [
   pastelFlowers,
@@ -23,18 +25,8 @@ const flowers = [
   blackFlowers
 ];
 
-
-// messageEmbed.setTitle('How can I help?').setColor(0x000000).setDescription(
-//   // Responses based on bet
-//   // Randomly generate Flower object & push to array
-//   // Read out data from Array foreach flower object
-//   // Format, or filter, Array objects into visuals (i.e flower images)
-//   // Send new formated messageEmbed to public gambling channel
-// );
-
 module.exports = async (msg, args) => {
   if (msg.content.startsWith('$ bid')) {
-
     msg.delete();
 
     let val1 = flowers[Math.floor(Math.random() * flowers.length)]
@@ -43,6 +35,24 @@ module.exports = async (msg, args) => {
     let val4 = flowers[Math.floor(Math.random() * flowers.length)]
     let val5 = flowers[Math.floor(Math.random() * flowers.length)]
 
-    msg.channel.send(`\n ${val1}\n ${val2}\n ${val3}\n ${val4}\n ${val5}\n`);
+    const plant1 = new MessageAttachment(val1)
+    const plant2 = new MessageAttachment(val2)
+    const plant3 = new MessageAttachment(val3)
+    const plant4 = new MessageAttachment(val4)
+    const plant5 = new MessageAttachment(val5)
+
+    msg.channel.send(`1 ... ${msg.author}`, { files: [plant1] });
+    msg.channel.send(`2 ... ${msg.author}`, { files: [plant2] });
+    msg.channel.send(`3 ... ${msg.author}`, { files: [plant3] });
+    msg.channel.send(`4 ... ${msg.author}`, { files: [plant4] });
+    msg.channel.send(`5 ... ${msg.author}`, { files: [plant5] });
+    
+    /**
+     * TODO: MessageButton for a choices
+     * Again
+     * Stop
+     * Double down
+     * PVP
+     */
   }
 }
